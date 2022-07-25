a3506b12587350ba0362c69ac15f480ff1f4e36d
vjdchvz <victorjhondechavez@gmail.com>
2022-07-26 00:26:29 +0800

    signUp,Login,Password reset authentication

--- fitneXt_App/app/src/main/java/com/example/fitnext_app/HOME.kt
+++ fitneXt_App/app/src/main/java/com/example/fitnext_app/HOME.kt
@@ -2,10 +2,19 @@ package com.example.fitnext_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class HOME : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        //receive  input
+        val inputValue = intent.getStringExtra("User")
+        //display  input
+        findViewById<TextView>(R.id.hometext).apply {
+            text = inputValue
+
+        }
     }
 }
--- fitneXt_App/app/src/main/java/com/example/fitnext_app/LoginActivity.kt
+++ fitneXt_App/app/src/main/java/com/example/fitnext_app/LoginActivity.kt
@@ -3,54 +3,69 @@ package com.example.fitnext_app
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
-import androidx.core.content.ContextCompat.startActivity
+import com.example.fitnext_app.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 
-class LoginActivity : AppCompatActivity()  {
+class LoginActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
-
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        val button3 = findViewById<Button>(R.id.button3)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        button3.setOnClickListener {
-           //call the function check
-            check()
-        }
-    }
-
-    private fun check() {
-        val inputting = findViewById<EditText>(R.id.userName)
-        val impasse = findViewById<EditText>(R.id.passWord)
-
-       //receive user input
-        val inputValue: String = inputting.text.toString()
-
-        //login conditions
-        if( impasse.text.toString() == "admin"){
-        //to proceed to next page
-            val page4 = Intent(this, LoginActivity2::class.java)
-            startActivity(page4)
-
-            //display user input
-            val intent = Intent(this@LoginActivity,LoginActivity2::class.java)
-            intent.putExtra("Username", "Welcome! $inputValue")
+        firebaseAuth = FirebaseAuth.getInstance()
+        // to sign  up
+        binding.textView15.setOnClickListener {
+            val intent = Intent(this, SignUp::class.java)
             startActivity(intent)
         }
-        else
-        {
-            Toast.makeText(this,"Login Failed", Toast.LENGTH_LONG).show()
+        //forgot password
+        binding.textView17.setOnClickListener {
+            val intent = Intent(this, ForgotPasswordActivity::class.java)
+            startActivity(intent)
+        }
+//login conditions
+        binding.button3.setOnClickListener {
+            val email = binding.l1.text.toString()
+            val pass = binding.l2.text.toString()
 
+            if (email.isNotEmpty() && pass.isNotEmpty()) {
+
+                firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        val intent = Intent(this, LoginActivity2::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this, "Password Incorrect !! ", Toast.LENGTH_SHORT).show()
+
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Empty Fields Are not Allowed !!", Toast.LENGTH_SHORT).show()
+
+            }
         }
     }
+
 }
+/*
+        override fun onStart() {
+            super.onStart()
+
+            if (firebaseAuth.currentUser != null) {
+                val intent = Intent(this, HOME::class.java)
+                startActivity (intent)
+            }
+        }
+    }
+*/
+
 
 
